7098a412b62352f99ffd58739f8b78bb7ba6011f
Taufique.Shaikh <taufique2010@gmail.com>
2016-06-16 15:07:36 +0530

    Changing master goal structure.

--- src/main/java/com/sungard/recycle/dto/MasterGoal.java
+++ src/main/java/com/sungard/recycle/dto/MasterGoal.java
@@ -1,7 +1,18 @@
 package com.sungard.recycle.dto;
 
-import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
 
 /**
  * Created by Rakesh.Sharma on 6/11/2016.
@@ -14,41 +25,84 @@ public class MasterGoal {
     @SequenceGenerator(name = "master_goal_gen", sequenceName = "master_goal_seq", allocationSize = 1, initialValue = 0)
     @Column(name = "id", nullable = false, updatable = false)
     private Long id;
-    private String name;
+    @Column(name="goal_name")
+    private String goalName;
     private String description;
-
+    private BigDecimal weightage;
+    @Column(name="expected_total")
+    private BigDecimal expectedTotal;
+    @OneToMany(cascade=CascadeType.ALL)
+    private List<MasterParameter> masterParameters = new ArrayList<MasterParameter>();
+    /**
+     * @return the id
+     */
     public Long getId() {
         return id;
     }
-
+    /**
+     * @param id the id to set
+     */
     public void setId(Long id) {
         this.id = id;
     }
-
-    public String getName() {
-        return name;
+    /**
+     * @return the goalName
+     */
+    public String getGoalName() {
+        return goalName;
     }
-
-    public void setName(String name) {
-        this.name = name;
+    /**
+     * @param goalName the goalName to set
+     */
+    public void setGoalName(String goalName) {
+        this.goalName = goalName;
     }
-
+    /**
+     * @return the description
+     */
     public String getDescription() {
         return description;
     }
-
+    /**
+     * @param description the description to set
+     */
     public void setDescription(String description) {
         this.description = description;
     }
-
+    /**
+     * @return the weightage
+     */
     public BigDecimal getWeightage() {
         return weightage;
     }
-
+    /**
+     * @param weightage the weightage to set
+     */
     public void setWeightage(BigDecimal weightage) {
         this.weightage = weightage;
     }
-
-    private BigDecimal weightage;
-
-}
+    /**
+     * @return the expectedTotal
+     */
+    public BigDecimal getExpectedTotal() {
+        return expectedTotal;
+    }
+    /**
+     * @param expectedTotal the expectedTotal to set
+     */
+    public void setExpectedTotal(BigDecimal expectedTotal) {
+        this.expectedTotal = expectedTotal;
+    }
+    /**
+     * @return the masterParameters
+     */
+    public List<MasterParameter> getMasterParameters() {
+        return masterParameters;
+    }
+    /**
+     * @param masterParameters the masterParameters to set
+     */
+    public void setMasterParameters(List<MasterParameter> masterParameters) {
+        this.masterParameters = masterParameters;
+    }
+    }
