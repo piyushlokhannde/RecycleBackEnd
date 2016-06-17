f54ac0ff0994c10ba9adada047a52214a3472785
mksmanjit <mksmanjit@gmail.com>
2016-06-17 11:33:27 +0530

    Code changes for Sprint Health Controller

---
+++ src/main/java/com/sungard/recycle/dto/DummyJasonObj.java
@@ -0,0 +1,16 @@
+package com.sungard.recycle.dto;
+
+/**
+ * Created by Manjit.Kumar on 6/17/2016.
+ */
+public class DummyJasonObj {
+    String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
